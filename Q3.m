9537549ecfa02bdb40684ed401e8abf90e3bc574
harsahibmatharoo <matharoh@mcmaster.ca>
2023-03-13 20:25:18 -0400

    Update Q3.m

--- Q3.m
+++ Q3.m
@@ -1,57 +1,59 @@
-function [scores_X, scores_Y, loadings_X, loadings_Y, weights_X, R2] = pls_nipals(X, Y, num_components)
+function [t, u, w_star, c, p, R2] = pls_nipals(X, Y, num_components)
 
 % center X and Y
 X_centered = X - mean(X);
 Y_centered = Y - mean(Y);
 
 % initialize arrays
-scores_X = zeros(size(X, 1), num_components);
-scores_Y = zeros(size(Y, 1), num_components);
-weights_X = zeros(size(X, 2), num_components);
-loadings_Y = zeros(size(Y, 2), num_components);
-loadings_X = zeros(size(X, 2), num_components);
+t = zeros(size(X, 1), num_components);
+u = zeros(size(Y, 1), num_components);
+w_star = zeros(size(X, 2), num_components);
+c = zeros(size(Y, 2), num_components);
+p = zeros(size(X, 2), num_components);
 R2 = zeros(1, num_components);
 
 for i = 1:num_components
     % initialize weight vector
-    weight = X_centered(:, 1);
+    w = X_centered(:, 1);
     
     % repeat until convergence
     while true
         % calculate score vector for X
-        score_X = X_centered * weight / norm(weight);
+        t_new = X_centered * w / norm(w);
         
         % calculate loading vector for Y
-        loading_Y = Y_centered' * score_X / (score_X' * score_X);
+        c_new = Y_centered' * t_new / (t_new' * t_new);
         
         % calculate score vector for Y
-        score_Y = Y_centered * loading_Y / (loading_Y' * loading_Y);
+        u_new = Y_centered * c_new / (c_new' * c_new);
         
         % calculate weight vector for X
-        weight_new = X_centered' * score_Y / (score_Y' * score_Y);
+        w_star_new = X_centered' * u_new / (u_new' * u_new);
         
         % check for convergence
-        if norm(weight_new - weight) < 1e-6
+        if norm(w_star_new - w) < 1e-6
             break;
         end
-        weight = weight_new;
+        w = w_star_new;
     end
     
     % calculate loading vector for X
-    loading_X = X_centered' * score_X / (score_X' * score_X);
+    p_new = X_centered' * t_new / (t_new' * t_new);
     
     % store results
-    scores_X(:, i) = score_X;
-    scores_Y(:, i) = score_Y;
-    weights_X(:, i) = weight / norm(weight);
-    loadings_Y(:, i) = loading_Y;
-    loadings_X(:, i) = loading_X / norm(loading_X);
+    t(:, i) = t_new;
+    u(:, i) = u_new;
+    w_star(:, i) = w / norm(w);
+    c(:, i) = c_new;
+    p(:, i) = p_new / norm(p_new);
     
     % calculate R^2
-    Yhat = scores_X * loadings_Y';
+    Yhat = t * c';
     R2(i) = 1 - sum(sum((Y_centered - Yhat).^2)) / sum(sum(Y_centered.^2));
     
     % update X and Y for next component
-    X_centered = X_centered - score_X * loading_X';
-    Y_centered = Y_centered - score_Y * loading_Y';
+    X_centered = X_centered - t_new * p_new';
+    Y_centered = Y_centered - u_new * c_new';
+end
+
 end
